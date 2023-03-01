5d26cabb907a215eae48c0c482cf3d1be93e4c62
Comba Yannick <comba.yannick@gmail.com>
2023-03-01 14:43:24 +0100

    connectPage

---
+++ api/connectDB.php
@@ -0,0 +1,8 @@
+<?php
+// Connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "julesimmobilier";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
