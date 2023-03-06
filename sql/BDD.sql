3ce1fabf03bdf289e2fb3992aa80585def68ef3e
Comba Yannick <comba.yannick@gmail.com>
2023-03-06 09:57:38 +0100

    Changement BDD

--- sql/BDD.sql
+++ sql/BDD.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE julesimmobilier
+-- CREATE DATABASE julesimmobilier
 
 CREATE TABLE profil (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -26,40 +26,46 @@ CREATE TABLE users (
     prenom VARCHAR(50),
     mail VARCHAR(50) UNIQUE,
     mdp VARCHAR(50),
+    team VARCHAR(6),
     id_profil INT,
     FOREIGN KEY (id_profil) REFERENCES profil(id)
 );
 
-INSERT INTO users (nom, prenom, mail, mdp, id_profil)
+INSERT INTO users (nom, prenom, mail, mdp, team, id_profil)
 VALUES (
     'Martin',
     'Jules',
     'jules.martin@julesimmobilier.fr',
     MD5('jaimelimmobilier123'),
+    'team 0',
     1
 ), (
     'Lebreton',
     'Loris',
     'loris.lebreton@julesimmobilier.fr',
     MD5('lasecretaire'),
+    'team 0',
     2
 ), (
     'Seigeot',
     'Maxence',
     'maxence.seigeot@julesimmobilier.fr',
     MD5('lacompta'),
+    'team 0',
     5
 ), (
     'Linget',
     'Evhan',
     'evhan.linget@julesimmobilier.fr',
     MD5('lagentdentretien'),
+    'team A',
     6
 ), (
     'Comba',
     'Yannick',
     'yannick.comba@julesimmobilier.fr',
     MD5('themanager'),
+    'team A',
     3
 );
 
@@ -153,4 +159,45 @@ VALUES (
     "oui",
     2,
     4
+);
+
+CREATE TABLE usersRigths (
+    id_profil INT,
+    statsALL BOOLEAN,
+    readALL BOOLEAN,
+    readTeam BOOLEAN,
+    FOREIGN KEY (id_profil) REFERENCES profil(id)
+);
+
+INSERT INTO usersRigths (id_profil, statsALL, readALL, readTeam)
+VALUES (
+    1,
+    1,
+    0,
+    0
+), (
+    2,
+    0,
+    1,
+    0
+), (
+    3,
+    0,
+    0,
+    1
+), (
+    4,
+    0,
+    0,
+    0
+), (
+    5,
+    0,
+    1,
+    0
+), (
+    6,
+    0,
+    0,
+    0
 );
