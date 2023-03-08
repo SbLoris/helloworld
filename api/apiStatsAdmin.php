533bdb40e48088195758623702e0bdab3e8118b5
Comba Yannick <comba.yannick@gmail.com>
2023-03-08 10:07:10 +0100

    Ajout RDV managers compta secretaire

--- api/apiStatsAdmin.php
+++ api/apiStatsAdmin.php
@@ -22,12 +22,11 @@ class statsAdmin {
         $this->rankingAgents = $this->rankingAgents();
     }
 
-    // Nombre de rendez-vous total pour l'agent connecté
     public function rdvAgentsManager() {
             if (isset($_SESSION['id_profil'])){
                 if ($_SESSION['id_profil'] == 3) {
                     $team = $_SESSION['team'];
-                    $sql = "SELECT RV.date_debut, RV.date_fin, CONCAT(C.nom, ' ', C.prenom), RV.adresse_rdv, CONCAT(U.nom, ' ', U.prenom) AS agent
+                    $sql = "SELECT RV.id, RV.date_debut, RV.date_fin, RV.commentaire, CONCAT(C.nom, ' ', C.prenom) as pnom_client, RV.adresse_rdv, CONCAT(U.nom, ' ', U.prenom) AS agent
                             FROM rendezvous RV
                             LEFT JOIN users U ON U.id = RV.id_user
                             LEFT JOIN clients C ON C.id = RV.id_client
@@ -36,9 +35,7 @@ class statsAdmin {
                             ORDER BY date_debut ASC;";
                     $result = $this->db->mysqli->query($sql);
 
-                    foreach ($result as $data) {
-                        return $_SESSION['rdvAgentsManager'] = $data;
-                    }
+                    return $result;
                 }
             }
     }
@@ -50,7 +47,7 @@ class statsAdmin {
                 $sql = "SELECT COUNT(*) AS rdvFini, CONCAT(U.nom, ' ', U.prenom) AS agent
                         FROM rendezvous RV
                         LEFT JOIN users U ON U.id = RV.id_user
-                        WHERE team = '$team'
+                        WHERE U.team = '$team'
                         AND id_statut_rdv = 2
                         GROUP BY id_user;";
                 $result = $this->db->mysqli->query($sql);
@@ -66,7 +63,7 @@ class statsAdmin {
     public function rdvAllAgents() {
         if (isset($_SESSION['id_profil'])){
             if ($_SESSION['id_profil'] == 2 || $_SESSION['id_profil'] == 5) {
-                $sql = "SELECT RV.date_debut, RV.date_fin, CONCAT(C.nom, ' ', C.prenom), RV.adresse_rdv, CONCAT(U.nom, ' ', U.prenom) AS agent
+                $sql = "SELECT RV.id, RV.date_debut, RV.date_fin, CONCAT(C.nom, ' ', C.prenom) as pnom_client, RV.adresse_rdv, RV.commentaire, CONCAT(U.nom, ' ', U.prenom) AS agent, U.team
                         FROM rendezvous RV
                         LEFT JOIN users U ON U.id = RV.id_user
                         LEFT JOIN clients C ON C.id = RV.id_client
@@ -75,9 +72,8 @@ class statsAdmin {
 
                 $result = $this->db->mysqli->query($sql);
 
-                foreach ($result as $data) {
-                    return $_SESSION['rdvAllAgents'] = $data;
-                }
+                return $result;
+                
             }
         }
     }
