bdc33f8d4fec37f6ff9b811306de42ffe61fd1bb
Comba Yannick <comba.yannick@gmail.com>
2023-03-08 16:09:35 +0100

    correction

--- header.php
+++ header.php
@@ -14,22 +14,14 @@
   <a href="#"><img src="img/logo.png" class="logo"></img></a>
   
   <div class="header-right">
-<<<<<<< HEAD
-  <a class="<?php if ($page == 'accueil') {echo 'active';} ?>" href="accueil.php">Accueil</a>
-  <a class="<?php if ($page == 'client') {echo 'active';} ?>" href="client.php">Clients</a>
-  <a href="accueil.php"><img src="img/deco.png" alt="Déconnexion"></a>
-</div>
-=======
-    <a class="active" href="#home">Home</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-    <?php 
+  <a class="class" href="accueil.php">Accueil</a>
+  <a class="client" href="client.php">Clients</a>
+  <?php 
       if ($_SESSION['id_profil'] == 1) {
         echo "<a href='equipe.php'>Gérer les équipes</a>";
       }
-    ?>
-    <a href="logout.php"> <img src="img/deco.png"></img></a>
-  </div>
->>>>>>> 8fda2e35cc8e49c4cc32ff211fcdb62bd948fa93
+  ?>
+  <a href="logout.php"><img src="img/deco.png" alt="Déconnexion"></a>
+</div>
 </div>
 </header>
