e2669e3722e48173cb2b193be1323abac5f204b2
Evhan Linget <evhan.linget@gmail.com>
2023-03-08 13:50:59 +0100

    accueil

--- accueil.php
+++ accueil.php
@@ -27,10 +27,111 @@ body {
 }
 
 
-.navbar {
+.menu {
     display: flex;
     flex-direction: column;
 }
+
+
+
+
+/* Début Page */
+
+.page .ligne {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 25px;
+    flex-wrap: wrap;
+}
+
+.page .ligne img {
+    margin: 40px;
+    border-radius: 10%;
+    cursor: pointer;
+    transition-duration: 1s;
+}
+
+.page .ligne img:hover {
+    transform: scale(1.5);
+}
+
+.produit img {
+    border-radius: 10%;
+    cursor: pointer;
+}
+
+.produit {
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+    font-family: Arial, Helvetica;
+}
+
+
+/*Fin Page*/
+
+.button {
+    text-align: center;
+    font-size: 1.3rem;
+    padding: 0.5rem;
+    color: #fff;
+    border-radius: 20px/50px;
+    text-decoration: none;
+    cursor: pointer;
+    background: #34495e;
+    transition: all 0.3s ease-out;
+}
+
+.button:hover {
+    background: #9C2F51;
+}
+
+.overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.7);
+    transition: opacity .4s;
+    visibility: hidden;
+    opacity: 0;
+}
+
+.overlay:target {
+    visibility: visible;
+    opacity: 1;
+}
+
+.popup {
+    text-align: center;
+    margin: 6rem auto;
+    padding: 2rem;
+    background: #fff;
+    border-radius: 5px;
+    width: 45%;
+    position: relative;
+    transition: all 0.4s ease-in-out;
+}
+
+.popup .cross {
+    position: absolute;
+    top: 1rem;
+    right: 1.5rem;
+    font-size: 2rem;
+    font-weight: bold;
+    text-decoration: none;
+    transition: 0.3s ease;
+    color: #333;
+    font-family: Arial, Helvetica;
+}
+
+.popup .cross:hover {
+    color: #12FCF8;
+}
+
+
 
 </style>
 
@@ -70,22 +171,36 @@ body {
 
 <div class="container">
   <canvas id="myChart"width="500" height="100"></canvas>
-  <div class="menu">
-  <ul class="navbar">
-    <li><a href="accueil.php"><img src="img/logo.png" alt=""></a></li>
-        <li><a href="#RDV">RDV</a>
-        <!--<ul>
-            <li><a href="#">Récents</a></li>
-            <li><a href="#">A venir</a></li>
-        </ul>--></li>
+  <ul class="menu">
+        <li><a href="#RDV">RDV</a></li>
         <li><a href="#stats">Statistiques</a></li>
         
         <li><a href="#">Planning</a></li>
         <li><a href="#">Feuilles de suivies</a></li>
-        <div class="deco">
-        <li id="deco"><a href="index_login.php"><img src="img/deco.png" alt=""></a></li></div>
     </ul>
-  </div>
+    <div class="ligne">
+            <div class="produit">
+                <img src="https://www.ville-palaiseau.fr/fileadmin/_processed_/b/4/csm_Bandeau_prendre_rendez-vous_ae8e2e2103.jpg" alt="">
+                <a href="#popup" class="button">Rendez-vous</a>
+
+                <div id="popup" class="overlay">
+                    <div class="popup">
+                        <h2>SWX-78</h2>
+                        <a href="#" class="cross">&times;</a>
+                        <img src="https://www.bmc-switzerland.com/media/catalog/product/cache/db486834651b8299de17894045ea7cde/b/m/bmc-22-10507-004-bmc-speedfox-al-two-mountain-bike-green-01.png" alt="" width="50%">
+                        <p>Un cadre en aluminium léger, avec un revêtement poudre durable haut de gamme et une géométrie dédiée aux cyclistes en herbe. Doté de roues 20", d’un groupe SRAM 8 vitesses et de freins TEKTRO, il inspire confiance avec son ergonomie
+                            adaptée. Simple. Fonctionnel. Le plaisir. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="produit">
+                <img src="https://medias.pourlascience.fr/api/v1/images/view/5a82ac868fe56f032c48000e/wide_1300/image.jpg" alt="">
+                <a href="#popup" class="button">Statistiques</a>
+
+                <div id="popup" class="overlay">
+                <canvas id="myChart"width="500" height="100"></canvas>
+                </div>
+            </div>
 </div>
 
 <div class="blank"></div>
